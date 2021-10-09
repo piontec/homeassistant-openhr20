9621252b2c2744f2562f8101fea39b51e7a3f2b7
Łukasz Piątkowski <piontec@gmail.com>
2021-10-09 10:37:07 +0200

    switch to 'ClimateEntity'

--- sensor.py
+++ sensor.py
@@ -6,8 +6,15 @@ from datetime import timedelta
 from typing import Any, Callable, Dict, Optional
 
 import voluptuous as vol
+from homeassistant.components.climate.const import (
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_OFF,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_OFF,
+    SUPPORT_TARGET_TEMPERATURE,
+)
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.climate import ClimateEntity
 import homeassistant.helpers.config_validation as cv
 
 from homeassistant.helpers.typing import (
@@ -55,9 +62,7 @@ async def async_setup_platform(
     async_add_entities(sensors, update_before_add=True)
 
 
-class OpenHR20Sensor(Entity):
-    """Representation of a GitHub Repo sensor."""
-
+class OpenHR20Sensor(ClimateEntity):
     def __init__(self, id: str, name: str, db_path: str) -> None:
         super().__init__()
         self.attrs: Dict[str, Any] = {}
@@ -67,28 +72,11 @@ class OpenHR20Sensor(Entity):
         self._available = True
         self._db_path = db_path
         self._attr_icon = "mdi:thermometer"
-
-    @property
-    def name(self) -> str:
-        """Return the name of the entity."""
-        return self._name
-
-    @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        return self._available
-
-    @property
-    def state(self) -> Optional[str]:
-        return self._state
-
-    @property
-    def device_state_attributes(self) -> Dict[str, Any]:
-        return self.attrs
-
-    @property
-    def unit_of_measurement(self) -> str:
-        return TEMP_CELSIUS
+        self._attr_temperature_unit = TEMP_CELSIUS
+        self._attr_precision = 0.01
+        self._attr_hvac_modes = [HVAC_MODE_HEAT]
+        self._attr_hvac_mode = HVAC_MODE_HEAT
+        self._attr_supported_features = SUPPORT_TARGET_TEMPERATURE
 
     async def async_update(self):
         async with aiosqlite.connect(self._db_path) as db:
@@ -98,14 +86,21 @@ class OpenHR20Sensor(Entity):
             ) as cursor:
                 async for row in cursor:
                     time = datetime.utcfromtimestamp(row[2])
-                    self.attrs["mode"] = row[3]
-                    self.attrs["valve"] = int(row[4])
-                    real_temp = float(row[5]) / 100
-                    self.attrs["wanted_temp"] = float(row[6]) / 100
-                    self.attrs["battery_v"] = float(row[7]) / 1000
-                    self.attrs["error"] = bool(row[8])
-                    self.attrs["window"] = bool(row[9])
-                    self.attrs["force"] = bool(row[10])
-                    self._state = real_temp
+                    self._attr_extra_state_attributes = {}
+                    self._attr_extra_state_attributes["mode"] = row[3]
+                    self._attr_extra_state_attributes["valve"] = int(row[4])
+                    self._attr_current_temperature = float(row[5]) / 100
+                    self._attr_target_temperature = float(row[6]) / 100
+                    self._attr_extra_state_attributes["battery_v"] = (
+                        float(row[7]) / 1000
+                    )
+                    self._attr_extra_state_attributes["error"] = bool(row[8])
+                    self._attr_extra_state_attributes["window"] = bool(row[9])
+                    self._attr_extra_state_attributes["force"] = bool(row[10])
+                    self._attr_hvac_action = (
+                        CURRENT_HVAC_OFF
+                        if self._attr_extra_state_attributes["valve"] == 0
+                        else CURRENT_HVAC_HEAT
+                    )
         # set available  = True if update more recent than 5 minutes
         # self._available = datetime.utcnow() - time <= timedelta(minutes=5)
