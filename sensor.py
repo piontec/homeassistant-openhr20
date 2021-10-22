c22ceb0759fa14918950dbcab1330cf28b935472
Łukasz Piątkowski <piontec@gmail.com>
2021-10-22 10:21:43 +0200

    dynamic icons and levels

--- sensor.py
+++ sensor.py
@@ -1,32 +1,22 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 import logging
+from sqlite3.dbapi2 import Row
 import aiosqlite
-import os.path
-import os
 
-from typing import Any, Callable, Dict, Optional
+from typing import Callable, Optional
 from homeassistant.helpers.entity import Entity
+from dataclasses import dataclass
 
-from voluptuous.error import Error
 from homeassistant import config_entries, core
-from homeassistant.components.climate.const import (
-    CURRENT_HVAC_HEAT,
-    CURRENT_HVAC_OFF,
-    HVAC_MODE_HEAT,
-    SUPPORT_TARGET_TEMPERATURE,
-)
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.components.climate import ClimateEntity
 
 from homeassistant.helpers.typing import (
-    ConfigType,
-    DiscoveryInfoType,
+    StateType,
 )
 
 from homeassistant.const import (
     CONF_FILE_PATH,
-    CONF_ID,
-    CONF_NAME,
+    ELECTRIC_POTENTIAL_VOLT,
+    PERCENTAGE,
     TEMP_CELSIUS,
 )
 
@@ -48,6 +38,62 @@ ohr20_values_mapping = [
 ]
 
 
+@dataclass
+class OpenHR20EntityInfo:
+    name: str
+    db_selector: Callable[[Row], StateType]
+    icon_setter: Callable[[StateType], str]
+    unit: Optional[str]
+
+
+ohr20_entities_info = [
+    OpenHR20EntityInfo("mode", lambda r: r[3], lambda _: "mdi:book-open", None),
+    OpenHR20EntityInfo(
+        "valve open", lambda r: int(r[4]), lambda _: "mdi:percent", PERCENTAGE
+    ),
+    OpenHR20EntityInfo(
+        "current temperature",
+        lambda r: float(r[5]) / 100,
+        lambda _: "mdi:thermometer",
+        TEMP_CELSIUS,
+    ),
+    OpenHR20EntityInfo(
+        "target temperature",
+        lambda r: float(r[6]) / 100,
+        lambda _: "mdi:thermometer",
+        TEMP_CELSIUS,
+    ),
+    OpenHR20EntityInfo(
+        "battery voltage",
+        lambda r: float(r[7]) / 1000,
+        lambda v: "mdi:battery"
+        if v and v > 2.4
+        else "mdi:battery-50"
+        if v and v > 2.2
+        else "mdi:battery-10",
+        ELECTRIC_POTENTIAL_VOLT,
+    ),
+    OpenHR20EntityInfo(
+        "error",
+        lambda r: bool(r[8]),
+        lambda v: "mdi:alert-circle" if v else "mdi:alert-circle-outline",
+        None,
+    ),
+    OpenHR20EntityInfo(
+        "window",
+        lambda r: bool(r[9]),
+        lambda v: "mdi:window-open-variant" if v else "mdi:window-closed-variant",
+        None,
+    ),
+    OpenHR20EntityInfo(
+        "force",
+        lambda r: bool(r[10]),
+        lambda v: "mdi:flash" if v else "mdi:flash-off",
+        None,
+    ),
+]
+
+
 async def async_setup_entry(
     hass: core.HomeAssistant,
     config_entry: config_entries.ConfigEntry,
@@ -59,8 +105,8 @@ async def async_setup_entry(
     thermo_id, thermo_name = config_entry.data[CONF_THERMO].split("=")
     db_path = config_entry.data[CONF_FILE_PATH]
     sensors = [
-        OpenHR20Sensor(thermo_id, thermo_name, db_path, m[0], m[1])
-        for m in ohr20_values_mapping
+        OpenHR20Sensor(thermo_id, thermo_name, db_path, ei)
+        for ei in ohr20_entities_info
     ]
     async_add_entities(sensors, update_before_add=True)
 
@@ -71,20 +117,20 @@ class OpenHR20Sensor(Entity):
         thermo_id: str,
         thermo_name: str,
         db_path: str,
-        sensor_name: str,
-        sensor_row: int,
+        entity_info: OpenHR20EntityInfo,
     ) -> None:
         super().__init__()
-        self.entity_description = f"OpenHR20 ID {thermo_id} - {sensor_name}"
-        self._attr_unique_id = f"{thermo_id}-{sensor_name}"
-        self._attr_name = sensor_name
+        self._attr_unique_id = f"{thermo_id}-{entity_info.name}"
+        self._attr_name = entity_info.name
         self._attr_state = None
         self._attr_available = True
         self._db_path = db_path
         self._thermo_id = thermo_id
         self._thermo_name = thermo_name
-        self._sensor_row = sensor_row
-        # self._attr_icon = "mdi:thermometer"
+        self._icon_setter = entity_info.icon_setter
+        self._attr_icon = entity_info.icon_setter(None)
+        self._attr_unit_of_measurement = entity_info.unit
+        self._db_selector = entity_info.db_selector
 
     async def async_update(self):
         async with aiosqlite.connect(self._db_path) as db:
@@ -93,7 +139,8 @@ class OpenHR20Sensor(Entity):
                 (self._thermo_id,),
             ) as cursor:
                 async for row in cursor:
-                    self._attr_state = row[self._sensor_row]
+                    self._attr_state = self._db_selector(row)
+                    self._attr_icon = self._icon_setter(self._attr_state)
                     # time = datetime.utcfromtimestamp(row[2])
         # set available  = True if update more recent than 5 minutes
         # self._available = datetime.utcnow() - time <= timedelta(minutes=5)
@@ -110,26 +157,3 @@ class OpenHR20Sensor(Entity):
             "model": "HR20",
             "sw_version": "0.1.1",
         }
-
-
-# async def async_setup_platform(
-#     hass: HomeAssistantError,
-#     config: ConfigType,
-#     async_add_entities: Callable,
-#     discovery_info: Optional[DiscoveryInfoType] = None,
-# ) -> None:
-#     _LOGGER.info("Starting openhr20")
-#     db_path = config[CONF_FILE_PATH]
-#     if not os.path.isfile(db_path):
-#         raise Error(f"DB file '{db_path}' not found")
-#     if not os.access(db_path, os.R_OK):
-#         raise Error(f"DB file '{db_path}' is not readable")
-#     if not os.access(db_path, os.W_OK):
-#         raise Error(f"DB file '{db_path}' is not writable")
-#     _LOGGER.debug(f"Trying to connect to sqlite DB at '{db_path}'.")
-#     async with aiosqlite.connect(db_path):
-#         _LOGGER.debug("DB connection successful")
-#     sensors = [
-#         OpenHR20Sensor(t[CONF_ID], t[CONF_NAME], db_path) for t in config[CONF_THERMO]
-#     ]
-#     # async_add_entities(sensors, update_before_add=True)
